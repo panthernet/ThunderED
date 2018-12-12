b46a399f21a83fc85e8ca47b04245339eb65fa27
Alexander Smirnov <panthernet@mail.ru>
2018-12-12 14:56:40 +0100

    Improved log feeder to send batch messages reducing Discord channel spam

--- ThunderED/Modules/OnDemand/SystemLogFeeder.cs
+++ ThunderED/Modules/OnDemand/SystemLogFeeder.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Timers;
 using ThunderED.Classes;
@@ -11,12 +14,33 @@ namespace ThunderED.Modules.OnDemand
         public override LogCat Category => LogCat.SLF;
         private static readonly Timer Timer;
         private static bool _isEnabled;
-
+        private static readonly object Locker = new object();
         static SystemLogFeeder()
         {
-            Timer = new Timer(5000) {AutoReset = false};
-            Timer.Elapsed += (sender, e) => { _isEnabled = true; };
+            _isEnabled = true;
+            Timer = new Timer(5000) {AutoReset = true};
+            Timer.Elapsed += (sender, e) =>
+            {
+                //_isEnabled = true;
+                lock (Locker)
+                {
+                    if (Package.Count > 0)
+                    {
+                        SendMessage().GetAwaiter().GetResult();
+                    }
+                }
+            };
             Timer.Start();
+        }
+
+        private static readonly ConcurrentBag<string> Package = new ConcurrentBag<string>();
+
+        private static async Task SendMessage()
+        {
+            var message = string.Join(Environment.NewLine, Package.ToArray());
+            await APIHelper.DiscordAPI.SendMessageAsync(SettingsManager.Settings.SystemLogFeederModule.DiscordChannelId, message);
+            Package.Clear();
+
         }
 
         public static async Task FeedMessage(string message, bool isCritical)
@@ -33,7 +57,9 @@ namespace ThunderED.Modules.OnDemand
                     Timer.Start();
                 }
 
-                await APIHelper.DiscordAPI.SendMessageAsync(SettingsManager.Settings.SystemLogFeederModule.DiscordChannelId, message);
+                if (Package.ToArray().Sum(a => a.Length) + message.Length >= 2000)
+                    await SendMessage();
+                Package.Add(message);
             }
             catch
             {
