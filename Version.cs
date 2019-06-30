ff030f8e8150520446bfb708d0ea992dfd6952cb
Alexander Smirnov <panthernet@mail.ru>
2019-06-30 17:51:55 +0200

    Version bump

--- Version.cs
+++ Version.cs
@@ -1,13 +1,13 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.16.0")]
-[assembly: AssemblyFileVersion("1.3.16.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
 
 namespace ThunderED
 {
     internal partial class Program
     {
-        public const string VERSION = "1.3.16";
+        public const string VERSION = "1.4.0";
     }
 }
 
