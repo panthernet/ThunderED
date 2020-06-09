5db93f1cf902c25933d8ecf9b414d0c0f5a210f0
Alexander Smirnov <panthernet@mail.ru>
2020-06-10 00:47:01 +0200

    Pushed version to 1.5.0

--- Version.cs
+++ Version.cs
@@ -1,13 +1,13 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.4.9.0")]
-[assembly: AssemblyFileVersion("1.4.9.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
 
 namespace ThunderED
 {
     internal partial class Program
     {
-        public const string VERSION = "1.4.9";
+        public const string VERSION = "1.5.0";
     }
 }
 
