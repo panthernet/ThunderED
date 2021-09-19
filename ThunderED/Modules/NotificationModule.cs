56104140bb5d09e5080163896b86fc0d32dc8522
Alexander Smirnov <panthernet@mail.ru>
2021-09-19 18:15:22 +0300

    Added StructureImpendingAbandonmentAssetsAtRisk notification support

--- ThunderED/Modules/NotificationModule.cs
+++ ThunderED/Modules/NotificationModule.cs
@@ -1270,6 +1270,28 @@ namespace ThunderED.Modules
                                                     await APIHelper.DiscordAPI.SendMessageAsync(discordChannel, mention, embed).ConfigureAwait(false);
                                                 }
                                                     break;
+                                                case "StructureImpendingAbandonmentAssetsAtRisk":
+                                                    {
+                                                        var daysLeft = Convert.ToInt32(GetData("daysUntilAbandon", data));
+                                                        var isCorp = Convert.ToBoolean(GetData("isCorpOwned", data));
+
+                                                        var stTypeName = structureType == null ? LM.Get("Structure") : structureType.name;
+                                                        var stName = structureNameDirect ?? LM.Get("Unknown");
+
+                                                        builder = new EmbedBuilder()
+                                                            .WithColor(new Color(0xff0000))
+                                                            .WithThumbnailUrl(Settings.Resources.ImgCitServicesOffline)
+                                                            .WithAuthor(author => author.WithName(LM.Get("StructureImpendingAbandonmentAssetsAtRiskMsg", stName, daysLeft)))
+                                                            .AddField(LM.Get("BlameCorp"), isCorp, true)
+                                                            .WithFooter($"EVE Time: {timestamp.ToShortDateString()} {timestamp.ToShortTimeString()}")
+                                                            .WithTimestamp(timestamp);
+                                                        embed = builder.Build();
+
+                                                        await APIHelper.DiscordAPI.SendMessageAsync(discordChannel, mention, embed).ConfigureAwait(false);
+
+                                                    }
+
+                                                    break;
                                             }
 
                                             //await SetLastNotificationId(notification.notification_id, null);                                            
