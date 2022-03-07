0cab3fba2db65a7648eef04f80a8c27c5f35c2ef
Alexander <panthernet@mail.ru>
2022-03-08 00:24:20 +0300

    Added map to CustomAssetsMap static folder for web custom assets

--- THDWebServer/Classes/WebUiHelper.cs
+++ THDWebServer/Classes/WebUiHelper.cs
@@ -9,6 +9,7 @@ namespace THDWebServer.Classes
     {
         private static readonly string DefaultAssetPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot", "Assets");
         private static readonly string CustomAssetPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot", "CustomAssets");
+        private static readonly string CustomMapAssetPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot", "CustomAssetsMap");
 
         static WebUiHelper()
         {
@@ -25,6 +26,11 @@ namespace THDWebServer.Classes
 
         public static string GetAsset(string name)
         {
+            var map = Path.Combine(CustomMapAssetPath, name);
+            if (File.Exists(map))
+            {
+                return $"/CustomAssetsMap/{name}";
+            }
             var def = Path.Combine(DefaultAssetPath, name);
             var cus = Path.Combine(CustomAssetPath, name);
             var asset = File.Exists(cus) ? $"/CustomAssets/{name}" : (File.Exists(def) ? $"/Assets/{name}" : null);
@@ -34,6 +40,11 @@ namespace THDWebServer.Classes
 
         public static string GetSvg(string name)
         {
+            var map = Path.Combine(CustomMapAssetPath, name);
+            if (File.Exists(map))
+            {
+                return $"/CustomAssetsMap/Svg/{name}";
+            }
             var def = Path.Combine(DefaultAssetPath, "Svg", name);
             var cus = Path.Combine(CustomAssetPath, "Svg", name);
             var asset = File.Exists(cus) ? $"/CustomAssets/Svg/{name}" : (File.Exists(def) ? $"/Assets/Svg/{name}" : null);
@@ -43,6 +54,11 @@ namespace THDWebServer.Classes
 
         public static string GetCustomHtmlContent(string name)
         {
+            var map = Path.Combine(CustomMapAssetPath, "Html", name);
+            if (File.Exists(map))
+            {
+                return File.ReadAllText(map);
+            }
             var cus = Path.Combine(CustomAssetPath, "Html", name);
             var asset = File.Exists(cus) ? File.ReadAllText(cus) : null;
             return asset;
